cdf337b48f468d992daeb9060d66eaab94cfda68
brian-nak <naklycky@usf.edu>
2021-10-23 12:06:31 -0400

    list applied jobs, save, list saved jobs, and remove saved jobs

--- db_connection.py
+++ db_connection.py
@@ -14,11 +14,12 @@ def createTables():
     cur = conn.cursor()
 
     cur.execute(f"CREATE TABLE IF NOT EXISTS auth(username VARCHAR(50) PRIMARY KEY, password VARCHAR(50) NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL);")
-    cur.execute(f"CREATE TABLE IF NOT EXISTS jobs (id SERIAL PRIMARY KEY, title VARCHAR(50) NOT NULL, description VARCHAR(255) NOT NULL, employer VARCHAR(50) NOT NULL, location VARCHAR(50) NOT NULL, name VARCHAR(50) NOT NULL, salary REAL);")
+    cur.execute(f"CREATE TABLE IF NOT EXISTS jobs(id SERIAL PRIMARY KEY, title VARCHAR(50) NOT NULL, description VARCHAR(255) NOT NULL, employer VARCHAR(50) NOT NULL, location VARCHAR(50) NOT NULL, name VARCHAR(50) NOT NULL, salary REAL);")
     cur.execute(f"CREATE TABLE IF NOT EXISTS control (username VARCHAR(50) PRIMARY KEY REFERENCES auth(username), email boolean, sms boolean , advertising boolean, language VARCHAR(20));")
     cur.execute(f"CREATE TABLE IF NOT EXISTS profile(username VARCHAR(50) PRIMARY KEY REFERENCES auth(username), title TEXT, major VARCHAR(70), university VARCHAR(70), about TEXT);")
     cur.execute(f"CREATE TABLE IF NOT EXISTS experiences(username VARCHAR(50), title VARCHAR(70), employer VARCHAR(70), date_started TEXT, date_ended TEXT, location VARCHAR(70), description TEXT);")
     cur.execute(f"CREATE TABLE IF NOT EXISTS education(username VARCHAR(50) PRIMARY KEY REFERENCES auth(username), school VARCHAR(70), degree VARCHAR(70), year_attended TEXT);")
     cur.execute(f"CREATE TABLE IF NOT EXISTS friends(user_1 VARCHAR(50), user_2 VARCHAR(50), pending BOOLEAN)")
-    cur.execute(f"CREATE TABLE IF NOT EXISTS applications(id SERIAL PRIMARY KEY, title VARCHAR(50), name VARCHAR(50), grad_date VARCHAR(50), start_date VARCHAR(50), reason VARCHAR(255))") #Challenge 6
+    #Challenge 6
+    cur.execute(f"CREATE TABLE IF NOT EXISTS applications(id SERIAL PRIMARY KEY, title VARCHAR(50), name VARCHAR(50), grad_date VARCHAR(50), start_date VARCHAR(50), reason VARCHAR(255)), save BOOLEAN") 
     conn.commit()
--- intership_page.py
+++ intership_page.py
@@ -8,7 +8,7 @@ from add_user import canAdd
 # Job Search option selected.
 def jobSearch(userName):
     print("-----------------------------------------")
-    print("Please select one of the three options")
+    print("Please select one of the four options")
     print("1.   Post Job")
     print("2.   Delete Job")
     print("3.   See Job List")
@@ -19,7 +19,7 @@ def jobSearch(userName):
     
     if(usr_input == 1):
         response = totalAccountsJobs()
-        if(canAdd(response)): # Check to make sure only 5 jobs created.
+        if(canAdd(response)): # Check to make sure only 10 jobs created.
             postJob(userName)
         else:
             print("\nToo many job posts currently\n")
@@ -64,7 +64,18 @@ def postJob(userName):
     jobSearch(userName)
     return 1
 
-#Challenge 6
+# This is not the same as totalAccount it is specific to jobs.
+def totalAccountsJobs():
+    conn = db_conn()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # counts number of users
+    cur.execute("SELECT COUNT(*) FROM jobs;")
+    return cur.fetchall()
+
+
+################Challenge 6################
 #Delete a job posted by the user.
 def deleteJob(userName):
     title = input("Please enter the job title: ")
@@ -95,10 +106,11 @@ def listJobs(userName):
         print("- " + elem[0])
 
     print("-----------------------------------------")
-    print("Please select one of the three options")
+    print("Please select one of the four options")
     print("1.   See Job Info")
     print("2.   Apply to Job")
-    print("3.   Go Back")
+    print("3.   Save Job")
+    print("4.   Go Back")
     print("-----------------------------------------")
     response = int(input("Please enter your selection:\t"))
     if(response == 1):
@@ -114,6 +126,12 @@ def listJobs(userName):
         else:
             print("Error in applying. You can't apply to a job you posted or doesn't exist.")
             listJobs(userName)
+    elif response == 3:
+        if saveJob(userName):
+            print("Job saved")
+        else:
+            print("Error saving job")
+            listJobs(userName)
     else:
         jobSearch(userName)
     return 1
@@ -128,11 +146,15 @@ def jobInfo(userName):
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM jobs WHERE title='{title}';")
     info = cur.fetchall()
+    cur.execute(f"SELECT * FROM applications WHERE title = '{title}' AND id = '{userName}';")
+    apps = cur.fetchall()
     print("Title: " + info[0][1])
     print("Description: " + info[0][2])
     print("Employer: " + info[0][3])
     print("Location: " + info[0][4])
     print("Salary: " + info[0][5])
+    if not apps[0][6]:
+        print("Already applied!")
     return 1
 
 #Challenge 6
@@ -149,18 +171,61 @@ def applyJob(userName):
         grad = input("Please enter your graduation date:\t")
         start = input("Please enter the date you wish to start:\t")
         reason = input("Please share why you'd be a good fit for this role:\t")
-        query = f"INSERT INTO applications (title, name, grad_date, start_date, reason) VALUES ('{title}', '{userName}', '{grad}', '{start}', '{reason}');"
+        query = f"INSERT INTO applications (title, name, grad_date, start_date, reason, pending) VALUES ('{title}', '{userName}', '{grad}', '{start}', '{reason}', FALSE);"
         cur.execute(query, (title, userName, grad, start, reason))
         conn.commit()
         return 1
 
 
-# This is not the same as totalAccount it is specific to jobs.
-def totalAccountsJobs():
-    conn = db_conn()
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
 
-    # counts number of users
-    cur.execute("SELECT COUNT(*) FROM jobs;")
-    return cur.fetchall()
+
+def listAppliedJobs(username):
+    print("Here are the jobs you have applied for")
+    conn = db_conn()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM applications WHERE name ='{username}';")
+    results = cur.fetchall()
+    for i in range(len(results)):
+        print(i,". ", results[i][1])
+
+def listNotAppliedJobs(username):
+    print("Here are the jobs you have not applied for")
+    conn = db_conn()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM applications WHERE name !='{username}';")
+    results = cur.fetchall()
+    for i in range(len(results)):
+        print(i,". ", results[i][1])
+
+def saveJob(username):
+    title = input("Please enter the title of the job you would like to save")
+    conn = db_conn()
+    cur = conn.cursor()
+    query = f"INSERT INTO applications (title, name, grad_date, start_date, reason, pending) VALUES ('{title}', '{username}', ' ', ' ', ' ', TRUE);"
+    cur.execute(query, (title, username, " ", " ", " "))
+    conn.commit()
+
+def applyToSavedJob(username):
+    print()
+
+
+def listSavedJobs(username):
+    print("Here are all your saved jobs")
+    conn = db_conn()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM applications WHERE id = '{username}' AND pending = TRUE;")
+    results = cur.fetchall()
+    for elements in results:
+        print(elements[1])
+    
+    usr_in = input("Enter 1 to go back or 2 to remove a saved job")
+    if usr_in == 1:
+        return 1
+    else:
+        title = input("Please enter the title of the job you'd like to remove")
+        cur.execute(f"DROP * FROM applications WHERE id = '{username} AND pending = TRUE AND title = '{title}'")
+        conn.commit()
+        print("Saved job removed")
+
+
+################End of Challenge 6################
